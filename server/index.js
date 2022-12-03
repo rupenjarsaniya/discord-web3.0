06af9d5bdb02455636b733eb87b235d7d1453d4b
rupen11 <jarsaniyarupen7984@gmail.com>
2022-12-03 14:22:03 +0530

    discord web3.0

--- server/index.js
+++ server/index.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(Gun.serve);
 
 app.get("/", (_, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
     res.status(200).send("> DEBUG: Discord Node is Live");
 });
 
@@ -21,3 +19,5 @@ const server = app.listen(port, () => {
 });
 
 Gun({ web: server });
+
+module.exports = app;
