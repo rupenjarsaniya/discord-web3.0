5b50b7912085f3994d4aab809f6efa4a1cf10429
rupen11 <jarsaniyarupen7984@gmail.com>
2022-12-03 14:37:14 +0530

    discord web3.0

--- server/index.js
+++ server/index.js
@@ -1,23 +1,42 @@
+// const express = require("express");
+// const Gun = require("gun");
+// const cors = require("cors");
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(Gun.serve);
+
+// app.get("/", (_, res) => {
+//     res.status(200).send("> DEBUG: Discord Node is Live");
+// });
+
+// const server = app.listen(port, () => {
+//     console.log(
+//         `> DEBUG: Discord Node is listening at http://localhost:${port}`
+//     );
+// });
+
+// Gun({ web: server });
+
+// module.exports = app;
 const express = require("express");
-const Gun = require("gun");
-const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = 4000;
 
-app.use(cors());
-app.use(Gun.serve);
-
-app.get("/", (_, res) => {
-    res.status(200).send("> DEBUG: Discord Node is Live");
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `);
 });
 
-const server = app.listen(port, () => {
-    console.log(
-        `> DEBUG: Discord Node is listening at http://localhost:${port}`
-    );
+app.get("/", (req, res) => {
+    res.send("Hey this is my API running 🥳");
 });
 
-Gun({ web: server });
+app.get("/about", (req, res) => {
+    res.send("This is my about route..... ");
+});
 
+// Export the Express API
 module.exports = app;
